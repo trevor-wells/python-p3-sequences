47ee7d609b2aac5bb94e0fd687cb9836d0a0d988
trevor wells <104236134+trevor-wells@users.noreply.github.com>
2023-05-02 16:10:36 -0600

    final commit

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib_list = []
+    for i in range(length):
+        if i == 0:
+            fib_list.append(0)
+        elif i == 1:
+            fib_list.append(1)
+        else:
+            fib_list.append(fib_list[i-2] + fib_list[i-1])
+    print(fib_list)
